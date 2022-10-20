64c998ae1bae09b51b0ab6b90cb1f3ee90dc1960
carlos agamez <oruz2019@gmail.com>
2022-10-19 21:07:41 -0500

    creacion de los modelos de facturas productos ventas

--- sales_page/sales/models.py
+++ sales_page/sales/models.py
@@ -1,12 +1,34 @@
 from email.policy import default
+from itertools import product
 from django.db import models
+from django.contrib.auth.models import User
 
-"""
-class Products(models.Model):# tabla
+class Invoice(models.Model):# tabla recivos
     # columna             # tpos de datos 
-    name = models.CharField(max_length=40)
+    id_client = models.ForeignKey(User, on_delete=models.CASCADE)
+    worth_invoice = models.IntegerField(default=0)
+    date_invoice = models.DateTimeField(" date invoice ")
+    status_payment = models.BooleanField(default=False)
+    
+    #def __str__(self):
+    #    return self.date_invoice
+
+
+class Product(models.Model):# tabla productos 
+    # columna             # tpos de datos 
+    name = models.CharField(max_length=20)
     units_available = models.IntegerField(default=0)
     worth_unit = models.IntegerField(default=0)
     
-    def __str__(self):
-        return self.name """
+    #def __str__(self):
+    #    return self.name 
+
+class Sale(models.Model):# tabla ventas
+    # columna             # tpos de datos 
+    id_invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    id_product = models.ForeignKey(product, on_delete=models.CASCADE)
+    units_product = models.IntegerField(default=0)
+    worth_sale = models.IntegerField(default=0)
+
+    #def __str__(self):
+    #    return self.id
